b0d02c078bdb33f5bba3b487adba5dffe8e277eb
RonaldoN71 <ronaldonandeibam7@gmail.com>
2025-11-16 13:25:02 +0530

    update the badge

--- frontend/src/components/ui/Badge.jsx
+++ frontend/src/components/ui/Badge.jsx
@@ -1,5 +1,3 @@
-
-
 export default function Badge({ children, className = "" }) {
   return (
     <span className={`inline-block text-xs px-2 py-1 rounded-full bg-purple-500 text-white ${className}`}>
