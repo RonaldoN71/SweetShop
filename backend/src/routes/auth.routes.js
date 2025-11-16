ce42bb0c954633afa6213fedf198fc4ca6b7ff6b
RonaldoN71 <ronaldonandeibam7@gmail.com>
2025-11-16 10:59:20 +0530

    chore: added simple route comments and cleaned structure in auth.routes.js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const authCtrl = require('../controllers/auth.controller');
 
+// user registration route
 router.post('/register', authCtrl.register);
+
+// user login route
 router.post('/login', authCtrl.login);
 
 module.exports = router;
