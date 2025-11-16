0d3e7049ff55ee6defb20ca8f08ae156e082e799
RonaldoN71 <ronaldonandeibam7@gmail.com>
2025-11-16 14:28:50 +0530

    update the cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,15 @@ const errorHandler = require("./middleware/error.middleware");
 const app = express();
 
 // Allow frontend to access backend
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://mithaas-khaki.vercel.app",  // Deployed frontend
+    "http://localhost:3000"             // Frontend (local)
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
 
 // Parse incoming JSON request bodies
 app.use(express.json());
